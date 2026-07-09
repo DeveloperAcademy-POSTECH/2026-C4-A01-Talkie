23a61efa995c001c16b3808048074f71b6d942b5
dongsun-kev <dongsun3658@gmail.com>
2026-07-10 01:32:03 +0900

    feat: 모델 만들기

---
+++ Talkie/Talkie/TalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkieApp.swift
+//  Talkie
+//
+//  Created by DS on 7/9/26.
+//
+
+import SwiftUI
+
+@main
+struct TalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
