ac83f7984c5b242967bfe0713a3c0c51b117124d
Grace <haeun2130@gmail.com>
2026-07-21 20:43:35 +0900

    design: 위젯설정방법UI 구현, app icon 추가

---
+++ Talkie/Talkie/Features/Widget/WidgetInstallUI.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetInstallUI.swift
+//  Talkie
+//
+//  Created by GRACE on 7/21/26.
+//
+
+import SwiftUI
+
+struct WidgetInstallUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WidgetInstallUI()
+}
