c01a8fb9ee63690de0716fa013d7fbcb3b1621c2
BK <music.bk@gmail.com>
2026-07-22 07:28:03 +0900

    feat: 가상 통화 Live Activity와 다이나믹 아일랜드 추가 (#30)

---
+++ Talkie/TalkieLiveActivity/TalkieLiveActivityBundle.swift
@@ -0,0 +1,15 @@
+//
+//  TalkieLiveActivityBundle.swift
+//  TalkieLiveActivity
+//
+
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct TalkieLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        FakeCallLiveActivityWidget()
+    }
+}
